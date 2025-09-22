37b8f7a90b3803dc756c9fdf2814e1ebc3892c65
gustavoszo <g_souza14@outlook.com>
2025-09-22 14:32:12 -0300

    Processando objeto de upload no s3

--- src/main/java/com/mycompany/lambdaFunction/Handler.java
+++ src/main/java/com/mycompany/lambdaFunction/Handler.java
@@ -2,28 +2,36 @@ package com.mycompany.lambdaFunction;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import jdk.internal.org.jline.terminal.TerminalBuilder;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
-import java.util.HashMap;
-import java.util.Map;
+public class Handler implements RequestHandler<S3Event, String> {
 
-public class Handler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+    private final S3Client s3 = S3Client.create();
 
     @Override
-    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-        Map<String, Object> response = new HashMap<>();
-        String variable = System.getenv("my_variable");
+    public String handleRequest(S3Event s3Event, Context context) {
+        S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
+        String bucketName = record.getS3().getBucket().getName();
+        String objectKey = record.getS3().getObject().getKey();
 
-        response.put("statusCode", 200);
-        response.put("body", event);
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(objectKey)
+                .build();
 
-        showMessage("Variavel de ambiente: " + variable);
-        showMessage("Event: " + event);
-        return response;
+        GetObjectResponse object = s3.getObject(getObjectRequest).response();
+
+        int mb = 1024 * 1024;
+        if (object.contentLength() > mb) { // Se o tamanho do objeto de upload for maior que 1mb
+            System.out.println("Objeto muito grande");
+            return "Objeto muito grande";
+        }
+
+        System.out.println("Objeto de tamanho ok");
+        return "Objeto de tamanho ok";
     }
-
-    private void showMessage(String message) {
-        System.out.println(message);
-    }
-
 }
